14c9bb887bccab3a29c60f7aa8cf3bba76bd36cb
pooya sharifi <pooyasharifi.sp2@gmail.com>
2020-01-29 15:00:39 -0800

    Add files via upload

---
+++ lr0.py
@@ -0,0 +1,9 @@
+# open table File 
+f = open("table.txt", "r")
+if f.mode == 'r':
+    string =f.read()
+string =string.replace(" ", "")
+string =string.replace(']','').replace('[','')
+string =string.replace("'",'').split(",")
+string = string[::2]
+print(string)
