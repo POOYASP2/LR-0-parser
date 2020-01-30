4fb687cffcef8a2a4a59c9399bc275c27fe0eca5
ariaieboy <smr.seddighy@gmail.com>
2020-01-30 20:41:26 +0330

    starting addint lr_parser_table

--- lr0.py
+++ lr0.py
@@ -6,4 +6,10 @@ string =string.replace(" ", "")
 string =string.replace(']','').replace('[','')
 string =string.replace("'",'').split(",")
 string = string[::2]
+lr_table = {
+    0:{
+        "if":['s',2]
+    }
+}
+print(string)
 
